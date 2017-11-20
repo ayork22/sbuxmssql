f1d03c9d59b2483a0307c8c1806c61ff6dff99e2
Alex York <ayork@newrelic.com>
2017-11-20 14:47:11 -0700

    GOOD

--- src/db.go
+++ src/db.go
@@ -1,1 +1,129 @@
-/Users/ayork/go/src/sbuxmssql/src/db.go
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"log"
+
+	_ "github.com/denisenkom/go-mssqldb"
+)
+
+var (
+	debug    = flag.Bool("debug", false, "enable debugging")
+	dbuser   = flag.String("dbuser", "sa", "the database user")
+	password = flag.String("password", "c0y0te#22", "the database password")
+	port     = flag.Int("port", 1433, "the database port")
+	server   = flag.String("server", "localhost", "the database server")
+)
+
+// SELECT (SELECT cntr_value * 100.00 FROM sys.dm_os_performance_counters WHERE counter_name = 'Buffer cache hit ratio') / (SELECT cntr_value FROM sys.dm_os_performance_counters WHERE counter_name = 'Buffer cache hit ratio base') AS BufferCacheHitRatio, (SELECT cntr_value FROM sys.dm_os_performance_counters WHERE counter_name = 'Page life expectancy' AND RTRIM([object_name]) LIKE '%:Buffer Manager') AS PageLife
+type memory struct {
+	buffercache int
+	pagelife    int
+}
+
+// SELECT d.Name AS DatabaseName, COUNT(c.connection_id) AS NumberOfConnections, ISNULL(SUM(c.num_reads), 0) AS NumberOfReads, ISNULL(SUM(c.num_writes), 0) AS NumberOfWrites FROM sys.databases d LEFT JOIN sys.sysprocesses s ON s.dbid = d.database_id LEFT JOIN sys.dm_exec_connections c ON c.session_id = s.spid WHERE (s.spid IS NULL OR c.session_id >= 51) GROUP BY d.Name
+type connections struct {
+	dbname       string
+	nConnections int
+	nReads       int
+	nWrites      int
+}
+
+// SELECT d.name AS DatabaseName, SUM(a.num_of_bytes_read) AS BytesRead, SUM(a.num_of_bytes_written) AS BytesWritten, SUM(a.size_on_disk_bytes) AS SizeInBytes, SUM(a.num_of_reads) AS NumberOfReads, SUM(a.num_of_writes) AS NumberOfWrites FROM sys.databases d LEFT JOIN sys.dm_io_virtual_file_stats(NULL, NULL) a ON d.database_id = a.database_id GROUP BY d.name ORDER BY d.name
+type fileio struct {
+	dbname       string
+	bytesread    int
+	byteswritten int
+	sizeinbytes  int
+	nReads       int
+	nWrites      int
+}
+
+//Calldb test
+func Calldb() (Messages []fileio) {
+	flag.Parse()
+
+	if *debug {
+		fmt.Printf(" dbuser:%s\n", *dbuser)
+		fmt.Printf(" password:%s\n", *password)
+		fmt.Printf(" port:%d\n", *port)
+		fmt.Printf(" server:%s\n", *server)
+	}
+
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d", *server, *dbuser, *password, *port)
+	if *debug {
+		fmt.Printf(" connString:%s\n", connString)
+	}
+	conn, err := sql.Open("mssql", connString)
+	fmt.Printf("***Connected***\n")
+	if err != nil {
+		log.Fatal("Open connection failed:", err.Error())
+	}
+
+	var (
+		sqlversion string
+	)
+	rows1, err := conn.Query("SELECT name FROM master..sysdatabases;")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for rows1.Next() {
+		err := rows1.Scan(&sqlversion)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// fmt.Printf("%v\n", sqlversion)
+	}
+
+	// rows, _ := conn.Query("SELECT d.name AS DatabaseName, SUM(a.num_of_bytes_read) AS BytesRead FROM sys.databases d LEFT JOIN sys.dm_io_virtual_file_stats(NULL, NULL) a ON d.database_id = a.database_id GROUP BY d.name ORDER BY d.name") // Note: Ignoring errors for brevity
+	rows, _ := conn.Query("SELECT d.name AS DatabaseName, SUM(a.num_of_bytes_read) AS BytesRead, SUM(a.num_of_bytes_written) AS BytesWritten, SUM(a.size_on_disk_bytes) AS SizeInBytes, SUM(a.num_of_reads) AS NumberOfReads, SUM(a.num_of_writes) AS NumberOfWrites FROM sys.databases d LEFT JOIN sys.dm_io_virtual_file_stats(NULL, NULL) a ON d.database_id = a.database_id GROUP BY d.name ORDER BY d.name") // Note: Ignoring errors for brevity
+	// var name1 string
+
+	var dn string
+	var br int
+	var bw int
+	var sb int
+	var nr int
+	var nw int
+
+	for rows.Next() {
+
+		if err := rows.Scan(&dn, &br, &bw, &sb, &nr, &nw); err != nil {
+			log.Fatal(err)
+		}
+		// fmt.Printf("***DatabaseName***: %s\n bytes: %d\n", dn, br)
+
+		s := fileio{
+			dbname:       dn,
+			bytesread:    br,
+			byteswritten: bw,
+			sizeinbytes:  sb,
+			nReads:       nr,
+			nWrites:      nw,
+		}
+
+		msg := fileio{
+			s.dbname,
+			s.bytesread,
+			s.byteswritten,
+			s.sizeinbytes,
+			s.nReads,
+			s.nWrites,
+		}
+		Messages = append(Messages, msg)
+
+	}
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	// rows, err := conn.Query("SELECT d.name AS DatabaseName, SUM(a.num_of_bytes_read) AS BytesRead FROM sys.databases d LEFT JOIN sys.dm_io_virtual_file_stats(NULL, NULL) a ON d.database_id = a.database_id GROUP BY d.name ORDER BY d.name")
+	// rows1, err := conn.Query("SELECT d.name AS DatabaseName, SUM(a.num_of_bytes_read) AS BytesRead, SUM(a.num_of_bytes_written) AS BytesWritten, SUM(a.size_on_disk_bytes)	AS SizeInBytes, SUM(a.num_of_reads)	AS NumberOfReads, SUM(a.num_of_writes) AS NumberOfWrites FROM sys.databases d LEFT JOIN sys.dm_io_virtual_file_stats(NULL, NULL) a ON d.database_id = a.database_id GROUP BY d.name ORDER BY d.name")
+
+	defer conn.Close()
+
+	fmt.Printf("***bye***\n")
+	return
+}
--- src/sbuxmssql.go
+++ src/sbuxmssql.go
@@ -1,1 +1,61 @@
-/Users/ayork/go/src/sbuxmssql/src/sbuxmssql.go
+package main
+
+import (
+	sdkArgs "github.com/newrelic/infra-integrations-sdk/args"
+	"github.com/newrelic/infra-integrations-sdk/log"
+	"github.com/newrelic/infra-integrations-sdk/metric"
+	"github.com/newrelic/infra-integrations-sdk/sdk"
+)
+
+type argumentList struct {
+	sdkArgs.DefaultArgumentList
+}
+
+const (
+	integrationName    = "com.newrelic.sbuxmssql"
+	integrationVersion = "0.1.0"
+)
+
+var args argumentList
+
+func populateInventory(inventory sdk.Inventory) error {
+	// Insert here the logic of your integration to get the inventory data
+	// Ex: inventory.SetItem("softwareVersion", "value", "1.0.1")
+	// --
+	return nil
+}
+
+func populateMetrics(ms *metric.MetricSet) error {
+	// Ex: ms.SetMetric("requestsPerSecond", 10, metric.GAUGE)
+
+	var fileio = Calldb()
+	for i := 0; i < len(fileio); i++ {
+		ms.SetMetric("BytesRead_"+fileio[i].dbname, fileio[i].bytesread, metric.GAUGE)
+		ms.SetMetric("BytesWritten_"+fileio[i].dbname, fileio[i].byteswritten, metric.GAUGE)
+		ms.SetMetric("SizeInBytes_"+fileio[i].dbname, fileio[i].sizeinbytes, metric.GAUGE)
+		ms.SetMetric("NumberReads_"+fileio[i].dbname, fileio[i].nReads, metric.GAUGE)
+		ms.SetMetric("NumberWrites_"+fileio[i].dbname, fileio[i].nWrites, metric.GAUGE)
+	}
+	return nil
+}
+
+func main() {
+	integration, err := sdk.NewIntegration(integrationName, integrationVersion, &args)
+	fatalIfErr(err)
+
+	if args.All || args.Inventory {
+		fatalIfErr(populateInventory(integration.Inventory))
+	}
+
+	if args.All || args.Metrics {
+		ms := integration.NewMetricSet("MSSQL")
+		fatalIfErr(populateMetrics(ms))
+	}
+	fatalIfErr(integration.Publish())
+}
+
+func fatalIfErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
