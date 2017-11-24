a8054f3b2d9d3213fd2f9a3427ae4f531f8ab1bb
Alex York <ayork@newrelic.com>
2017-11-24 09:19:10 -0700

    dynamichostname

--- src/dbconnect.go
+++ src/dbconnect.go
@@ -5,17 +5,26 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/denisenkom/go-mssqldb"
 )
 
 var (
-	debug         = flag.Bool("debug", false, "enable debugging")
+	debug         = flag.Bool("debug", true, "enable debugging")
 	password      = flag.String("password", "test", "the database password")
 	port     *int = flag.Int("port", 1433, "the database port")
 	server        = flag.String("server", "DESKTOP-J0L7P9N\\SQLEXPRESS", "the database server")
-	user          = flag.String("user", "test", "the database user")
+	dbuser        = flag.String("dbuser", "test", "the database user")
 )
+
+// var (
+// 	debug         = flag.Bool("debug", true, "enable debugging")
+// 	password      = flag.String("password", "c0y0te#22", "the database password")
+// 	port     *int = flag.Int("port", 1433, "the database port")
+// 	server        = flag.String("server", "localhost", "the database server")
+// 	dbuser        = flag.String("dbuser", "sa", "the database user")
+// )
 
 var conn *sql.DB
 
@@ -27,14 +36,22 @@ func DBconnect() (conn *sql.DB) {
 		fmt.Printf(" password:%s\n", *password)
 		fmt.Printf(" port:%d\n", *port)
 		fmt.Printf(" server:%s\n", *server)
-		fmt.Printf(" user:%s\n", *user)
+		fmt.Printf(" dbuser:%s\n", *dbuser)
 	}
 
-	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d", *server, *user, *password, *port)
+	hostname, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	// Windows the DB Server name is needed as well
+	hostname = hostname + "\\SQLEXPRESS"
+	fmt.Println("OS Hostname:", hostname)
+
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d", hostname, *dbuser, *password, *port)
 	if *debug {
 		fmt.Printf(" connString:%s\n", connString)
 	}
-	conn, err := sql.Open("mssql", connString)
+	conn, err = sql.Open("mssql", connString)
 	if err != nil {
 		log.Fatal("Open connection failed:", err.Error())
 	}
